c253b951930559d2ebd0ffd7c109930bbcbf098a
Richard Kumahia <Richard_Kumahia@student.uml.edu>
2021-06-27 05:41:45 -0400

    fixed some mistakes in initializing variables

--- Labs/lab2/matrix_add_mult/host/src/main.cpp
+++ Labs/lab2/matrix_add_mult/host/src/main.cpp
@@ -3,13 +3,28 @@
 #include <stdlib.h>
 #include <math.h>
 #include <cstring>
-//#include "CL/opencl.h"
-//#include "AOCLUtils/aocl_utils.h"
+#include "CL/opencl.h"
+#include "AOCLUtils/aocl_utils.h"
+
+using namespace aocl_utils;
 
 int m = 200;
 int n = 400;
 int p = 600;
 
+// OpenCL runtime configuration
+static cl_platform_id platform = NULL;
+static cl_device_id device = NULL;
+static cl_context context = NULL;
+static cl_command_queue queue = NULL;
+static cl_kernel kernel = NULL;
+static cl_program program = NULL;
+
+// Control whether the emulator should be used.
+static bool use_emulator = false;
+
+// Function prototypes
+bool init();
 void cleanup();
 static void device_info_ulong( cl_device_id device, cl_device_info param, const char* name);
 static void device_info_uint( cl_device_id device, cl_device_info param, const char* name);
